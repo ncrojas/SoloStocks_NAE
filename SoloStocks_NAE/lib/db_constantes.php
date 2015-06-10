9b1df6c71b41470ce94c2bf1d552532106382094
evillagra <eavc00@vtr.net>
2015-06-09 20:54:30 -0400

    commit completo

---
+++ SoloStocks_NAE/lib/db_constantes.php
@@ -0,0 +1,8 @@
+<?php
+/*Definición opciones de conexión*/
+define("MYSQL_SERVER","mysql:host=localhost");
+define("MYSQL_USER","root");
+define("MYSQL_PASS","");
+
+define("MYSQL_DB","dbname=DEFINIR");
+?>
