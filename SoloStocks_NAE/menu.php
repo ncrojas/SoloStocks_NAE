322609a2dd9db241fcf5419b3233780f92b799eb
evillagra <eavc00@vtr.net>
2015-06-13 17:28:56 -0400

    varios

--- SoloStocks_NAE/menu.php
+++ SoloStocks_NAE/menu.php
@@ -1,11 +1,27 @@
 <div id='cssmenu'>
 <ul>
 <li class='active'><a href='index.php'>Inicio</a></li>
-<li class='has-sub'><a href='#'>Solo Stock</a>
+<li class='has-sub'><a href='#'>Mantenedor Usuario</a>
 	<ul> 
 	<li ><a href='usuario_add.php'>Agregar</a></li>
 	<li ><a href='usuario_upd.php'>Actualizar</a></li>
 	<li ><a href='usuario_del.php'>Eliminar</a></li>
+	</ul>
+</li>
+
+<li class='has-sub'><a href='#'>Mantenedor Producto</a>
+	<ul> 
+	<li ><a href='producto_add.php'>Agregar</a></li>
+	<li ><a href='producto_upd.php'>Actualizar</a></li>
+	<li ><a href='producto_del.php'>Eliminar</a></li>
+	</ul>
+</li>
+
+<li class='has-sub'><a href='#'>Mantenedor Proveedor</a>
+	<ul> 
+	<li ><a href='proveedor_add.php'>Agregar</a></li>
+	<li ><a href='proveedor_upd.php'>Actualizar</a></li>
+	<li ><a href='proveedor_del.php'>Eliminar</a></li>
 	</ul>
 </li>
 <li><a href='#'>Contactanos</a></li>
