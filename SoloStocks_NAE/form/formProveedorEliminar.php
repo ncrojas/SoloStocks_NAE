e4a3fc69a8cd638312f07a7e36d34bcd5a179b01
ncrojas <ne.rojasb@alumnos.duoc.cl>
2015-06-28 14:17:22 -0300

    Detalles en eliminar proveedor

--- SoloStocks_NAE/form/formProveedorEliminar.php
+++ SoloStocks_NAE/form/formProveedorEliminar.php
@@ -2,18 +2,35 @@
 $oProveedor=new Proveedor();
 ?>
 <form method="post" action="accform/accProveedorEliminar.php">
+	<table style="width:700px;">
+		<tr>
+			<td colspan="5" height="30" valign="middle" align="center" style="color:#ffffff;background-color:#ff5512;">Eliminar Proveedor(es)</td>
+		</tr>
+		<tr style="background-color:#d8d8b1;">
+			<td>&nbsp;</td>
+			<td>Id</td>
+			<td>Nombre</td>
+			<td>Direcci&oacute;n</td>
+			<td>Pa&iacute;s</td>
+		</tr>
 <?php
-While($Registro=$oProveedor->Selecciona()){?>
-	
-<input type="checkbox" name="elimina<?=$Registro->getIdProveedor()?>" value="<?=$Registro->getIdProveedor()?>">
-
-
-
-<?=$Registro->getNombre()?>/<?=$Registro->getPais()?>
-<br>
+	While($Registro=$oProveedor->Selecciona()){?>
+		<tr>
+			<td>
+				<input type="checkbox" name="elimina<?=$Registro->getIdProveedor()?>" value="<?=$Registro->getIdProveedor()?>">
+			</td>
+			<td><?=$Registro->getIdProveedor()?></td>
+			<td><?=$Registro->getNombre()?></td>
+			<td><?=$Registro->getDireccion()?></td>
+			<td><?=$Registro->getPais()?></td>
+		</tr>
 <?php
-}
-
+	}
 ?>
-<input type="submit" value="Eliminar">
+		<tr>
+			<td colspan="5" height="40" valign="bottom" align="center">
+				<input type="submit" id="btneliminar" value="Eliminar" />
+			</td>
+		</tr>
+	</table>
 </form>
