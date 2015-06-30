4fb27ec395fcb553e96672241c6003d5accdb936
ncrojas <ne.rojasb@alumnos.duoc.cl>
2015-06-30 18:48:47 -0300

    .

--- SoloStocks_NAE/constante.php
+++ SoloStocks_NAE/constante.php
@@ -1,4 +1,8 @@
 <?php
 define("PATHAPP",$_SERVER['DOCUMENT_ROOT']."/SoloStocks_nae/");
 define("TITULOAPP","SOLO STOCK");
+define('GMAIL','ne.rojasb@alumnos.duoc.cl');
+define('GNAME','Nelson Rojas');
+define('GUSR','ne.rojasb@alumnos.duoc.cl');
+define('GPWD',''); // Colocar clave válida
 ?>
